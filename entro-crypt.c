c90776b39ca34533005dd9464aac37d60824d774
Will Parsons <entrocraft@gmail.com>
2024-01-10 10:02:23 +0000

    Deleting subsequent function call support by default.

--- entro-crypt.c
+++ entro-crypt.c
@@ -3,14 +3,14 @@
 
 void entro_crypt(const char *input, uint32_t *entropy) {
   if (input[0] != 0) {
-    entropy[0] += entro_hash(input, entropy[0]) + 1;
-    entropy[1] += (entro_hash(input, entropy[0]) + 1) ^ entropy[0];
-    entropy[2] += (entro_hash(input, entropy[1]) + 1) ^ entropy[1];
-    entropy[3] += (entro_hash(input, entropy[2]) + 1) ^ entropy[2];
-    entropy[4] += (entro_hash(input, entropy[3]) + 1) ^ entropy[3];
-    entropy[5] += (entro_hash(input, entropy[4]) + 1) ^ entropy[4];
-    entropy[6] += (entro_hash(input, entropy[5]) + 1) ^ entropy[5];
-    entropy[7] += (entro_hash(input, entropy[6]) + 1) ^ entropy[6];
+    entropy[0] = entro_hash(input, 0) + 1;
+    entropy[1] = (entro_hash(input, entropy[0]) + 1) ^ entropy[0];
+    entropy[2] = (entro_hash(input, entropy[1]) + 1) ^ entropy[1];
+    entropy[3] = (entro_hash(input, entropy[2]) + 1) ^ entropy[2];
+    entropy[4] = (entro_hash(input, entropy[3]) + 1) ^ entropy[3];
+    entropy[5] = (entro_hash(input, entropy[4]) + 1) ^ entropy[4];
+    entropy[6] = (entro_hash(input, entropy[5]) + 1) ^ entropy[5];
+    entropy[7] = (entro_hash(input, entropy[6]) + 1) ^ entropy[6];
     entropy[0] ^= entropy[7];
     entropy[1] ^= entropy[0];
     entropy[2] ^= entropy[1];
--- test.c
+++ test.c
@@ -2,12 +2,10 @@
 #include "entro-crypt.h"
 
 int main(void) {
-  const char *input_1 = "message_1";
-  const char *input_2 = "message_2";
+  const char *input = "message";
   uint32_t entropy[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-  entro_crypt(input_1, entropy);
-  entro_crypt(input_2, entropy);
+  entro_crypt(input, entropy);
   printf("%08X%08X%08X%08X%08X%08X%08X%08X\n", entropy[0], entropy[1], entropy[2], entropy[3], entropy[4], entropy[5], entropy[6], entropy[7]);
   return 0;
 }
