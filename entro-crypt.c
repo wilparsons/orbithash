52e84a776c77af429b851088d1e34b50ead3330a
Will Parsons <entrocraft@gmail.com>
2024-02-01 20:40:15 +0000

    Adding speed performance improvements with new Entro Hash.

--- entro-crypt.c
+++ entro-crypt.c
@@ -2,14 +2,57 @@
 #include "entro-hash.h"
 
 void entro_crypt(const char *input, uint32_t *entropy) {
-  entropy[0] = entro_hash(input, 0) + 1;
-  entropy[1] = (entro_hash(input, entropy[0]) + 2) ^ entropy[0];
-  entropy[2] = (entro_hash(input, entropy[1]) + 3) ^ entropy[1];
-  entropy[3] = (entro_hash(input, entropy[2]) + 4) ^ entropy[2];
-  entropy[4] = (entro_hash(input, entropy[3]) + 5) ^ entropy[3];
-  entropy[5] = (entro_hash(input, entropy[4]) + 6) ^ entropy[4];
-  entropy[6] = (entro_hash(input, entropy[5]) + 7) ^ entropy[5];
-  entropy[7] = (entro_hash(input, entropy[6]) + 8) ^ entropy[6];
+  char auxiliary[4];
+  unsigned long i = 0;
+
+  entropy[0] = 0;
+
+  while (i != 7) {
+    if (input[i] != 0) {
+      auxiliary[0] = input[i];
+
+      if (input[i + 1] != 0) {
+        auxiliary[1] = input[i + 1];
+
+        if (input[i + 2] != 0) {
+          auxiliary[2] = input[i + 2];
+          auxiliary[3] = 0;
+        } else {
+          auxiliary[2] = 0;
+        }
+      } else {
+        auxiliary[1] = 0;
+      }
+    } else {
+      auxiliary[0] = 0;
+    }
+
+    i++;
+    entropy[i] = entro_hash(auxiliary, entropy[i - 1]);
+  }
+
+  i = 0;
+
+  while (input[i] != 0) {
+    auxiliary[0] = input[i];
+
+    if (input[i + 1] != 0) {
+      auxiliary[1] = input[i + 1];
+
+      if (input[i + 2] != 0) {
+        auxiliary[2] = input[i + 2];
+        auxiliary[3] = 0;
+      } else {
+        auxiliary[2] = 0;
+      }
+    } else {
+      auxiliary[1] = 0;
+    }
+
+    entropy[i & 7] += entro_hash(auxiliary, entropy[(i + 2) & 7]) ^ entropy[(i + 1) & 7];
+    i++;
+  }
+
   entropy[0] ^= entropy[7];
   entropy[1] ^= entropy[0];
   entropy[2] ^= entropy[1];
@@ -17,4 +60,5 @@ void entro_crypt(const char *input, uint32_t *entropy) {
   entropy[4] ^= entropy[3];
   entropy[5] ^= entropy[4];
   entropy[6] ^= entropy[5];
+  entropy[7] ^= entropy[6];
 }
