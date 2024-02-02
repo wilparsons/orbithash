ce98aac8590114f6e7c32e6f9d53b13b9e089063
Will Parsons <entrocraft@gmail.com>
2024-02-02 00:30:52 +0000

    Adding lowercase hexadecimal output to the test.c file.

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int main(void) {
   uint32_t entropy[8];
 
   entro_crypt(input, entropy);
-  printf("%08X%08X%08X%08X%08X%08X%08X%08X\n", entropy[0], entropy[1], entropy[2], entropy[3], entropy[4], entropy[5], entropy[6], entropy[7]);
+  printf("%08x%08x%08x%08x%08x%08x%08x%08x\n", entropy[0], entropy[1], entropy[2], entropy[3], entropy[4], entropy[5], entropy[6], entropy[7]);
   return 0;
 }
