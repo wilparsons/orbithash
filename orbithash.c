10241a02e28ca854ce18f8bf23225552b91de2fb
Wil Parsons <entrocraft@gmail.com>
2024-02-22 18:08:07 -0800

    Adding better chained digest finalization.

--- orbithash.c
+++ orbithash.c
@@ -69,12 +69,19 @@ void orbithash(const char *input, uint32_t *entropy) {
   }
 
   entropy[0] += entropy[7];
-  entropy[1] += entropy[0];
+  entropy[1] ^= entropy[0];
   entropy[2] += entropy[1];
-  entropy[3] += entropy[2];
+  entropy[3] ^= entropy[2];
   entropy[4] += entropy[3];
-  entropy[5] += entropy[4];
+  entropy[5] ^= entropy[4];
   entropy[6] += entropy[5];
+  entropy[7] ^= entropy[6];
+  entropy[0] ^= entropy[7];
+  entropy[1] += entropy[0];
+  entropy[2] ^= entropy[1];
+  entropy[3] += entropy[2];
+  entropy[4] ^= entropy[3];
+  entropy[5] += entropy[4];
+  entropy[6] ^= entropy[5];
   entropy[7] += entropy[6];
-  entropy[0] += entropy[7];
 }
