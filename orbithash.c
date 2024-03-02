bb0988fb01089d3b28381b073d148562fea3c03f
Wil Parsons <entrocraft@gmail.com>
2024-03-02 00:54:55 -0800

    Modifying indices in finalization.

--- orbithash.c
+++ orbithash.c
@@ -54,6 +54,6 @@ void orbithash(const char *input, uint32_t *entropy) {
   entropy[3] += ((entropy[4] ^ i) >> 6) + entropy[2] + i;
   entropy[2] += ((entropy[3] ^ i) >> 5) + entropy[1] + i;
   entropy[1] += ((entropy[2] ^ i) >> 4) + entropy[0] + i;
-  entropy[0] += ((entropy[7] ^ i) >> 3) + entropy[7] + i;
-  entropy[7] += ((entropy[0] ^ i) >> 2) + entropy[0] + i;
+  entropy[0] += ((entropy[7] ^ i) >> 3) + entropy[1] + i;
+  entropy[7] += ((entropy[0] ^ i) >> 2) + entropy[2] + i;
 }
