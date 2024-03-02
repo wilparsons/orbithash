9b3dc5c46427300d95f3c0f5c89b1f805527fe1d
Wil Parsons <entrocraft@gmail.com>
2024-03-02 10:10:52 -0800

    Adding input and auxiliary variable to finalization.

--- orbithash.c
+++ orbithash.c
@@ -48,12 +48,12 @@ void orbithash(const char *input, uint32_t *entropy) {
   entropy[5] ^= (entropy[4] + entropy[3]) << 4;
   entropy[6] += (entropy[5] + entropy[4]) << 3;
   entropy[7] ^= (entropy[6] + entropy[5]) << 2;
-  entropy[6] += ((entropy[7] ^ i) >> 9) + entropy[5] + i;
-  entropy[5] += ((entropy[6] ^ i) >> 8) + entropy[4] + i;
-  entropy[4] += ((entropy[5] ^ i) >> 7) + entropy[3] + i;
-  entropy[3] += ((entropy[4] ^ i) >> 6) + entropy[2] + i;
-  entropy[2] += ((entropy[3] ^ i) >> 5) + entropy[1] + i;
-  entropy[1] += ((entropy[2] ^ i) >> 4) + entropy[0] + i;
-  entropy[0] += ((entropy[7] ^ i) >> 3) + entropy[1] + i;
-  entropy[7] += ((entropy[0] ^ i) >> 2) + entropy[2] + i;
+  entropy[6] += ((entropy[(input[0] + entropy[6] + i) & 7] + (entropy[7] ^ i) + copied_entropy + i) >> 8) + entropy[5] + copied_entropy + i;
+  entropy[5] += ((entropy[(input[0] + entropy[5] + i) & 7] + (entropy[6] ^ i) + copied_entropy + i) >> 7) + entropy[4] + copied_entropy + i;
+  entropy[4] += ((entropy[(input[0] + entropy[4] + i) & 7] + (entropy[5] ^ i) + copied_entropy + i) >> 7) + entropy[3] + copied_entropy + i;
+  entropy[3] += ((entropy[(input[0] + entropy[3] + i) & 7] + (entropy[4] ^ i) + copied_entropy + i) >> 6) + entropy[2] + copied_entropy + i;
+  entropy[2] += ((entropy[(copied_entropy + i) & 7] + (entropy[3] ^ i) + copied_entropy + i) >> 5) + entropy[1] + copied_entropy + i;
+  entropy[1] += ((entropy[(input[0] + copied_entropy + i) & 7] + (entropy[2] ^ i) + copied_entropy + i) >> 4) + entropy[0] + copied_entropy + i;
+  entropy[0] += ((entropy[(input[0] + copied_entropy) & 7] + (entropy[7] ^ i) + copied_entropy + i) >> 3) + entropy[1] + copied_entropy + i;
+  entropy[7] += ((entropy[(input[0] + i) & 7] + (entropy[0] ^ i) + copied_entropy + i) >> 2) + entropy[2] + copied_entropy + i;
 }
